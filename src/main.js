084348cec97540d97ce10bd2232f5aab4ca46c6e
zlobin_ilya <qwertin1998@gmail.com>
2021-08-19 23:59:31 +0300

    Add plugins file and router vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import { OhVueIcon } from "./js/plugins/index";
+import router from "@/js/router/index.js";
+Vue.component("v-icon", OhVueIcon);
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    router,
+    render: (h) => h(App),
+}).$mount("#app");
